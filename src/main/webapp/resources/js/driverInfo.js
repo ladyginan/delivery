a2522ff202d29d433c22e35d970c2fd8e9835707
Irina <irishkinson@list.ru>
2018-06-01 15:12:37 +0300

    spring security stackoverflow

---
+++ src/main/webapp/resources/js/driverInfo.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    var  driverId = getDriverId();
+    loadDriver(driverId);
+});
+
+var loadedDriver;
+
+function getDriverId() {
+    var pathArray = window.location.pathname;
+    pathArray = pathArray.replace('.html','').split('/');
+    var driverId = pathArray[pathArray.length - 1];
+    return driverId;
+}
+
+function loadDriver(driverId) {
+    $.get("/driverLogin/" + driverId, function (driver) {
+
+        loadedDriver = driver;
+
+        $("#idDriver").val(driverId);
+        $("#waggonRegNumber").val(driver.waggon);
+        $("#orderNumber").val(driver.orderNumber);
+
+    })
+}
+
+function loadCompanions() {
+    $get("/driverLogin/companions",function(companions){
+        var companions = $('#companions');
+        for(companion in companions){
+            companions
+                .append(
+                    $('<tr />')
+                        .val(companions[companion].idCompanion)
+                        .text(
+                            companions[companion].fullname
+                        )
+
+                )
+        }
+    })
+}
