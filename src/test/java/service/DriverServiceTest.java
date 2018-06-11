5b80aecf993c1085bbdc2c876c884f8ba0f8291e
Irina <irishkinson@list.ru>
2018-06-11 18:27:13 +0300

    tests driver service

--- src/test/java/service/DriverServiceTest.java
+++ src/test/java/service/DriverServiceTest.java
@@ -4,6 +4,7 @@ import com.app.model.Driver;
 import com.app.model.Enums.DriverStatus;
 import com.app.model.Waggon;
 import com.app.repository.impl.DriversRepository;
+import com.app.service.impl.DriverService;
 import org.hibernate.SessionFactory;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,33 +16,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DriverServiceTest {
     @Mock
-    SessionFactory factory;
-    @Mock
-    Map city;
-    @Mock
-    Waggon waggon;
-
     DriversRepository driversRepository;
+//    @Mock
+////    Map city;
+////    @Mock
+////    Waggon waggon;
+    @Mock
     Driver driver;
+    DriverService driverService;
 
     @Before
-    public void before(){
+    public void before() {
         MockitoAnnotations.initMocks(this);
-        this.driversRepository = new DriversRepository(factory);
-        driver = new Driver(5858,"Vasya","Petrov",8,DriverStatus.DRIVING, city, waggon );
-    }
-
-    @Test
-    public void driverServiceAddDriver(){
-        this.driversRepository.addDriver(driver);
-        int idDriver = driver.getIdDriver();
-        Driver driverFromDB = driversRepository.getDriver(idDriver);
-        assertEquals(idDriver, driverFromDB.getIdDriver());
+        this.driverService = new DriverService();
+        Driver driver = new Driver();
     }
 
     @Test( expected = NullPointerException.class )
     public void whenCreateIsTriggeredForNullEntity_thenException(){
-        this.driversRepository.addDriver(null);
+        this.driverService.addDriver(null);
     }
+    @Test
+    public void driverServiceAddDriverTest() {
+        this.driverService.addDriver(driver);
+    }
+
+
+//        this.city = new Map();
+//        this.waggon = new Waggon();
+//        driver = new Driver(5858,"Vasya","Petrov",8,DriverStatus.DRIVING, city, waggon );
+
+
+//
+//    @Test
+//    public void driverServiceAddDriver(){
+//        this.driversRepository.addDriver(driver);
+//        int idDriver = driver.getIdDriver();
+//        Driver driverFromDB = driversRepository.getDriver(idDriver);
+//        assertEquals(idDriver, driverFromDB.getIdDriver());
+//    }
+//
 
 }
